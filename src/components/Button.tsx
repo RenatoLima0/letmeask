8728a87878adcca4737736120ef522e5e00c04eb
RenatoLima0 <renatoslima46@gmail.com>
2021-06-21 11:45:36 -0300

    configuração do firebase

---
+++ src/components/Button.tsx
@@ -0,0 +1,22 @@
+// type ButtonProps = {
+//   children?: string,
+// }
+
+import { useState } from "react";
+
+// export function Button (props: ButtonProps) {
+export function Button () {
+  // let counter = 0;
+  const [counter, setCounter] = useState(0)
+  
+  function increment() {
+    // counter += 1;
+    setCounter(counter + 1)
+    console.log(counter);
+  }
+
+  return (
+    // <button>{props.children || 'Default'}</button>
+    <button onClick={increment}>{counter}</button>
+  )
+}
